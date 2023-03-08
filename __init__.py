2ddf4ec74d788b3ad913faf8f4de4698f8bd96fc
lakshmivishal9496 <113028705+lakshmivishal9496@users.noreply.github.com>
2023-03-08 12:47:55 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append(r'C:\\Users\\laksh\\Dice_Game\\src')
-"""src"""
-"""dice.py"""
-"""dice.py"""
-"""dicehand.py"""
-"""__init___.py"""
